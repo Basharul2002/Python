433b29316f98db3c7e252139d7d19b1e704049f9
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-09-17 06:28:38 +0600

    Added Simple Project -> Stock Price.py

---
+++ Simple Project/Stock Price.py
@@ -0,0 +1,12 @@
+# pip install yfinance
+
+
+import yfinance as yf
+
+STK = input("Enter share name: ").upper()
+
+data yf.Ticker (STK).history (period="1d")
+
+last_market_price = data['Close').iloc[-1]
+
+print(f"Last market price {STK):", last_market_price)
